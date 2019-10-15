c25c976a7c556fcdbe9159d3ec76c5d793acacd9
Alexey <grant744@yandex.ru>
2019-10-15 20:08:31 +0300

    Branch update

---
+++ js/logic.js
@@ -0,0 +1,41 @@
+
+    // button "more_details" processing work
+		var button_status = 0;
+
+		$('.more_details').click(function(){
+
+				if (button_status == 1) {
+						// if button is activated
+						button_status = 0;
+
+            $('.section_3').css({
+								'height' : '350px'
+						});
+
+            $('.for_shadow').css({
+                'box-shadow' : '0px -100px 50px 0px #fff inset'
+            });
+
+            $('.more_details').css({
+                'background-color' : '#fff',
+                'color' : '#3366FF'
+            });
+
+				} else {
+						// if button is deactivated
+						button_status = 1;
+
+						$('.section_3').css({
+								'height' : '500px'
+						});
+
+            $('.for_shadow').css({
+                'box-shadow' : '0px 0px 0px 0px #fff inset'
+            });
+
+            $('.more_details').css({
+                'background-color' : '#3366FF',
+                'color' : '#fff'
+            });
+				}
+		});
